c2d8558f00b438cae736a9f8cadeba0ecd36f98d
mohid921 <103675068+mohid921@users.noreply.github.com>
2023-11-20 15:41:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Register from "./pages/register/page";
 function App() {
   return (
     <>
+      <h1>It is working</h1>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<p>Home</p>} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
